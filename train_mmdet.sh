60758440a927a4fa20cc88dd9ccbd44b360b7d18
pprp <1115957667@qq.com>
2022-03-17 17:24:35 +0800

    update ssd res50

--- train_mmdet.sh
+++ train_mmdet.sh
@@ -7,4 +7,7 @@ source activate pool
 # bash tools/dist_train.sh ./configs/retinanet/retinanet_r50_fpn_1x_coco.py 2
 # modify the learning rate for faster rcnn. default gpus=8 our gpus=4
 # bash tools/dist_train.sh ./configs/faster_rcnn/faster_rcnn_r50_rf_fpn_1x_coco.py 4
-bash tools/dist_train.sh ./configs/centernet/centernet_resnet18_140e_coco.py 8 
+# bash tools/dist_train.sh ./configs/centernet/centernet_resnet18_140e_coco.py 8 
+
+bash tools/dist_train.sh ./configs/ssd/ssd_res50_scratch_600e_coco.py 1
+
