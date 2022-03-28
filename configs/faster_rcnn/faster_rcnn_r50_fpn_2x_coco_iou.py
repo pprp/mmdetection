4ff47086b919df9110c01a5df7cbf0ed8b9eb5de
pprp <1115957667@qq.com>
2022-03-28 15:29:40 +0800

    update ohem and iou

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_2x_coco_iou.py
@@ -0,0 +1,11 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+model = dict(
+    train_cfg=dict(rcnn=dict(sampler=dict(type='OHEMSampler')))
+    roi_head=dict(
+        bbox_head=dict(
+            reg_decoded_bbox=True,
+            loss_bbox=dict(type='BoundedIoULoss', loss_weight=10.0))))
